e9bf71c58cfc3f9ad923fbc0435c2c8b3163e6af
Nishant Ranjan Verma <nishantranjanverma@gmail.com>
2024-08-17 20:30:15 +0100

    structure

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 from typing import List
 
-HYPEN_E_DOT='-e .'
+# HYPEN_E_DOT='-e .'
 
-def get_requiremet(file_path:str)->List[str]:
-    requirements = []
-    with open(file_path) as f:
-        requirements=f.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+# def get_requiremet(file_path:str)->List[str]:
+#     requirements = []
+#     with open(file_path) as f:
+#         requirements=f.readlines()
+#         requirements=[req.replace("\n","")for req in requirements]
         
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
+#         if HYPEN_E_DOT in requirements:
+#             requirements.remove(HYPEN_E_DOT)
+#     return requirements
    
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
@@ -19,7 +19,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 __version__ = "0.0.6"
 REPO_NAME = "MongoDB-Connector-Pkg"
-PKG_NAME= "MongoDB-Connect"
+PKG_NAME= "MongoDB-Connect-Pkg"
 AUTHOR_USER_NAME = "nishantrv"
 AUTHOR_EMAIL = "nishantranjanverma@gmail.com"
 
@@ -37,6 +37,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requiremet("/requirements_dev.txt")
+   # install_requires = get_requiremet("/requirements_dev.txt")
     
 )
